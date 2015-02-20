447ef738100c0dc93fc519d3710469c28928371c
Tim Caswell <tim@creationix.com>
2015-02-20 16:18:07 -0600

    Fix make.bat to build win64 only

--- make.bat
+++ make.bat
@@ -6,27 +6,17 @@ IF NOT "x%1" == "x" GOTO :%1
 GOTO :build
 
 :static
-ECHO "Building static"
-cmake -DWithOpenSSL=ON -DWithSharedOpenSSL=OFF -DWithZLIB=ON -DWithSharedZLIB=OFF -H. -Bbuild
-GOTO :end
-
-:static64
 ECHO "Building static64"
-cmake -DWithOpenSSL=ON -DWithSharedOpenSSL=OFF -DWithZLIB=ON -DWithSharedZLIB=OFF -H. -Bbuild  -G"Visual Studio 12 2013 Win64"
+cmake -DWithOpenSSL=ON -DWithSharedOpenSSL=OFF -DWithZLIB=ON -DWithSharedZLIB=OFF -H. -Bbuild  -G"Visual Studio 12 Win64"
 GOTO :end
 
 :tiny
-ECHO "Building tiny"
-cmake -H. -Bbuild
-GOTO :end
-
-:tiny64
 ECHO "Building tiny64"
-cmake -H. -Bbuild -G"Visual Studio 12 2013 Win64"
+cmake -H. -Bbuild -G"Visual Studio 12 Win64"
 GOTO :end
 
 :build
-IF NOT EXIST build CALL Make.bat large
+IF NOT EXIST build CALL Make.bat static
 cmake --build build --config Release -- /maxcpucount
 COPY build\Release\luvi.exe .
 GOTO :end
@@ -69,14 +59,6 @@ IF EXIST build RMDIR /S /Q build
 IF EXIST luvi.exe DEL /F /Q luvi.exe
 GOTO :end
 
-:publish
-ECHO "Building all versions"
-git submodule update --init --recursive
-CALL make.bat clean
-CALL make.bat tiny64
-CALL make.bat test
-GOTO :end
-
 :reset
 git submodule update --init --recursive
 git clean -f -d
@@ -85,14 +67,14 @@ GOTO :end
 
 :publish-tiny
 CALL make.bat reset
-CALL make.bat tiny64
+CALL make.bat tiny
 CALL make.bat test
 github-release upload --user luvit --repo luvi --tag %LUVI_TAG% --file luvi.exe --name luvi-tiny-Windows-amd64.exe
 GOTO :end
 
 :publish-static
 CALL make.bat reset
-CALL make.bat static64
+CALL make.bat static
 CALL make.bat test
 github-release upload --user luvit --repo luvi --tag %LUVI_TAG% --file luvi.exe --name luvi-static-Windows-amd64.exe
 GOTO :end
