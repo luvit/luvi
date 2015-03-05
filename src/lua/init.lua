202f779a6d715e693068698806657dc9fae0fae6
Tim Caswell <tim@creationix.com>
2015-03-05 15:43:09 -0600

    Bake bundle.action into luvi itself

--- src/lua/init.lua
+++ src/lua/init.lua
@@ -17,8 +17,13 @@ limitations under the License.
 --]]
 
 local os = require('ffi').os
+local env = require('env')
+local uv = require('uv')
 
 local getPrefix, splitPath, joinParts
+
+local tmpBase = os == "Windows" and (env.get("TMP") or uv.cwd()) or
+                                    (env.get("TMPDIR") or '/tmp')
 
 if os == "Windows" then
   -- Windows aware path utils
@@ -350,6 +355,25 @@ return function(args)
   end
 
   local function commonBundle(bundle)
+
+    function bundle.action(path, action, ...)
+      -- If it's a real path, run it directly.
+      if uv.fs_access(path, "r") then return action(path) end
+      -- Otherwise, copy to a temporary folder and run from there
+      local data, err = bundle.readfile(path)
+      if not data then return nil, err end
+      local dir = assert(uv.fs_mkdtemp(pathJoin(tmpBase, "lib-XXXXXX")))
+      path = pathJoin(dir, path:match("[^/\\]+$"))
+      local fd = uv.fs_open(path, "w", 384) -- 0600
+      uv.fs_write(fd, data, 0)
+      uv.fs_close(fd)
+      local success, ret = pcall(action, path, ...)
+      uv.fs_unlink(path)
+      uv.fs_rmdir(dir)
+      assert(success, ret)
+      return ret
+    end
+
     luvi.bundle = bundle
     luvi.makeBundle = makeBundle
 
