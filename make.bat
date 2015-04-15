2442d7959637340f1d90c51510072a41c6db623e
Tim Caswell <tim@creationix.com>
2015-04-15 15:29:47 -0500

    Fix test in windows make.bat

--- make.bat
+++ make.bat
@@ -23,7 +23,7 @@ GOTO :end
 
 :test
 IF NOT EXIST luvi.exe CALL Make.bat
-luvi.exe samples\test.app 1 2 3 4
+luvi.exe samples\test.app -- 1 2 3 4
 luvi.exe samples\test.app -o test.exe
 test.exe 1 2 3 4
 DEL /Q test.exe
