756b6bc3837806c1598aaa609192c5e2c9e60005
Tim Caswell <tim@creationix.com>
2015-07-29 17:13:17 -0500

    Disable cpp build and add utf8 support to static pcre

--- deps/pcre.cmake
+++ deps/pcre.cmake
@@ -11,7 +11,10 @@ else (WithSharedPCRE)
       "Default limit on internal looping. See MATCH_LIMIT in config.h.in for details.")
   OPTION(PCRE_BUILD_PCREGREP "Build pcregrep" OFF)
   OPTION(PCRE_BUILD_TESTS    "Build the tests" OFF)
-  # set (PCRE_BUILD__8 ON CACHE)
+  OPTION(PCRE_BUILD_PCRECPP "Build the PCRE C++ library (pcrecpp)." OFF)
+  SET(PCRE_SUPPORT_UTF ON CACHE BOOL
+      "Enable support for Unicode Transformation Format (UTF-8/UTF-16/UTF-32) encoding.")
+
   include_directories(${CMAKE_BINARY_DIR}/deps/pcre)
   add_subdirectory(deps/pcre)
   message("Enabling Static PCRE")
