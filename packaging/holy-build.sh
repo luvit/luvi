badfee1a1e474c8484564584c5dc2da52be40466
Ryan Liptak <squeek502@hotmail.com>
2019-12-13 16:50:18 -0800

    Fix all symbols being stripped in holy-build.sh

--- packaging/holy-build.sh
+++ packaging/holy-build.sh
@@ -21,6 +21,6 @@ make ${BUILD_TYPE}
 make -j${NPROCS}
 ldd build/luvi
 libcheck build/luvi
-# holy-build-box adds -g to CFLAGS, so we need to strip
-strip --strip-all build/luvi
+# holy-build-box adds -g to CFLAGS, so we need to strip debug symbols
+strip --strip-debug build/luvi
 cp build/luvi /io
