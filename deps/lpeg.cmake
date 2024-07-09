c65e147cf3a463ac4f28f2fb5f75e89cebc344ac
Sinister Rectus <sinister.rectus@gmail.com>
2024-07-08 21:57:23 -0400

    Adds new lpeg files to cmake config

--- deps/lpeg.cmake
+++ deps/lpeg.cmake
@@ -3,6 +3,7 @@ set(LPEGLIB_DIR "${CMAKE_CURRENT_SOURCE_DIR}/deps/lpeg" CACHE PATH "Path to lpeg
 add_library(lpeglib STATIC
   ${LPEGLIB_DIR}/lpcap.c
   ${LPEGLIB_DIR}/lpcode.c
+  ${LPEGLIB_DIR}/lpcset.c
   ${LPEGLIB_DIR}/lpprint.c
   ${LPEGLIB_DIR}/lptree.c
   ${LPEGLIB_DIR}/lpvm.c
